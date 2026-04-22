6442e31d92143be64274c502e0de25c8b15cedf5
abdullahzaky1 <128037121+abdullahzaky1@users.noreply.github.com>
2026-04-22 07:37:09 +0200

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,44 @@
+# 1. Importing extensions
+import streamlit as st
+import google.generativeai as ai
+
+# 2. Gemini API setup
+api_key = st.secrets["GEMINI_API_KEY"]
+
+model_name = 'gemini-3.1-flash-lite-preview'
+ai.configure(api_key=api_key)
+model = ai.GenerativeModel(model_name=model_name)
+# 3. Page title
+st.title('Chat with our AI✨', text_alignment='center')
+st.subheader('Ask our AI anything about nutrition facts, food, etc.')
+
+# 4. Taking the user question
+question = st.chat_input('Enter your question...', 
+                         accept_audio=True, accept_file=True)
+
+if question: # If the question area is not empty
+    # 1st chatting identity
+    with st.chat_message('human'):
+        st.write(question)
+    
+    # 2nd chatting identity 
+    with st.chat_message('ai'):
+        with st.spinner(text='Generating...✨'):
+            prompt = f'''
+            Answer this question:
+            {question}
+            But consider the following:
+            - Do not answer any question irrelevant to food and 
+            nutrition.
+            - This is the menu of our restaurant:
+                menu = [
+            'Chicken':250,
+            'Margerita':210, 
+            'Pepperoni':280,
+            'Burger':320
+                ]
+            - Working hours: 8:00 AM to 12:AM
+            '''
+            answer = model.generate_content(prompt)
+        st.write(answer.text)
+        
